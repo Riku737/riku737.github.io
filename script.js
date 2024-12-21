cd824443439a8bb2ab6d89d52f4211995027cc99
Riki <rmcal024@uottawa.ca>
2024-12-21 10:49:25 -0500

    Added projects hover react

---
+++ script.js
@@ -0,0 +1,39 @@
+document.getElementById("thumbnail_highlight").addEventListener("mouseover", event => {
+    document.getElementById("highlight_cover").style.transform = "scale(1.1)"
+});
+
+document.getElementById("thumbnail_highlight").addEventListener("mouseout", event => {
+    document.getElementById("highlight_cover").style.transform = "scale(1)"
+});
+
+document.getElementById("thumbnail_1").addEventListener("mouseover", event => {
+    document.getElementById("thumbnail_image_1").style.transform = "scale(1.1)"
+});
+
+document.getElementById("thumbnail_1").addEventListener("mouseout", event => {
+    document.getElementById("thumbnail_image_1").style.transform = "scale(1)"
+});
+
+document.getElementById("thumbnail_2").addEventListener("mouseover", event => {
+    document.getElementById("thumbnail_image_2").style.transform = "scale(1.1)"
+});
+
+document.getElementById("thumbnail_2").addEventListener("mouseout", event => {
+    document.getElementById("thumbnail_image_2").style.transform = "scale(1)"
+});
+
+document.getElementById("thumbnail_3").addEventListener("mouseover", event => {
+    document.getElementById("thumbnail_image_3").style.transform = "scale(1.1)"
+});
+
+document.getElementById("thumbnail_3").addEventListener("mouseout", event => {
+    document.getElementById("thumbnail_image_3").style.transform = "scale(1)"
+});
+
+document.getElementById("thumbnail_4").addEventListener("mouseover", event => {
+    document.getElementById("thumbnail_image_4").style.transform = "scale(1.1)"
+});
+
+document.getElementById("thumbnail_4").addEventListener("mouseout", event => {
+    document.getElementById("thumbnail_image_4").style.transform = "scale(1)"
+});
