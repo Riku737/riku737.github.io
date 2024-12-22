4298f65a53e083d36346a148be84883dc09827de
Riki <rmcal024@uottawa.ca>
2024-12-22 17:06:54 -0500

    Added about section, added video hover effect to project section

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-document.getElementById("thumbnail_highlight").addEventListener("mouseover", event => {
-    document.getElementById("highlight_cover").style.transform = "scale(1.1)"
+// Projects section
+document.getElementById("thumbnail_highlight").addEventListener("mouseenter", () => {
+    document.getElementById("highlight_cover").play();
+    document.getElementById("highlight_cover").style.transform = "scale(1.05)";
 });
 
-document.getElementById("thumbnail_highlight").addEventListener("mouseout", event => {
+document.getElementById("thumbnail_highlight").addEventListener("mouseleave", () => {
+    document.getElementById("highlight_cover").pause();
+    document.getElementById("highlight_cover").load()
     document.getElementById("highlight_cover").style.transform = "scale(1)"
+    
 });
 
 document.getElementById("thumbnail_1").addEventListener("mouseover", event => {
-    document.getElementById("thumbnail_image_1").style.transform = "scale(1.1)"
+    document.getElementById("thumbnail_image_1").style.transform = "scale(1.05)"
 });
 
 document.getElementById("thumbnail_1").addEventListener("mouseout", event => {
@@ -15,7 +20,7 @@ document.getElementById("thumbnail_1").addEventListener("mouseout", event => {
 });
 
 document.getElementById("thumbnail_2").addEventListener("mouseover", event => {
-    document.getElementById("thumbnail_image_2").style.transform = "scale(1.1)"
+    document.getElementById("thumbnail_image_2").style.transform = "scale(1.05)"
 });
 
 document.getElementById("thumbnail_2").addEventListener("mouseout", event => {
@@ -23,7 +28,7 @@ document.getElementById("thumbnail_2").addEventListener("mouseout", event => {
 });
 
 document.getElementById("thumbnail_3").addEventListener("mouseover", event => {
-    document.getElementById("thumbnail_image_3").style.transform = "scale(1.1)"
+    document.getElementById("thumbnail_image_3").style.transform = "scale(1.05)"
 });
 
 document.getElementById("thumbnail_3").addEventListener("mouseout", event => {
@@ -31,42 +36,9 @@ document.getElementById("thumbnail_3").addEventListener("mouseout", event => {
 });
 
 document.getElementById("thumbnail_4").addEventListener("mouseover", event => {
-    document.getElementById("thumbnail_image_4").style.transform = "scale(1.1)"
+    document.getElementById("thumbnail_image_4").style.transform = "scale(1.05)"
 });
 
 document.getElementById("thumbnail_4").addEventListener("mouseout", event => {
     document.getElementById("thumbnail_image_4").style.transform = "scale(1)"
 });
-
-document.getElementById("experience_1").addEventListener("mouseover", event => {
-    document.getElementById("experience_1").style.transform = "scale(1.005)"
-});
-
-document.getElementById("experience_1").addEventListener("mouseout", event => {
-    document.getElementById("experience_1").style.transform = "scale(1)"
-});
-
-document.getElementById("experience_2").addEventListener("mouseover", event => {
-    document.getElementById("experience_2").style.transform = "scale(1.005)"
-});
-
-document.getElementById("experience_2").addEventListener("mouseout", event => {
-    document.getElementById("experience_2").style.transform = "scale(1)"
-});
-
-document.getElementById("experience_3").addEventListener("mouseover", event => {
-    document.getElementById("experience_3").style.transform = "scale(1.005)"
-});
-
-document.getElementById("experience_3").addEventListener("mouseout", event => {
-    document.getElementById("experience_3").style.transform = "scale(1)"
-});
-
-document.getElementById("experience_4").addEventListener("mouseover", event => {
-    document.getElementById("experience_4").style.transform = "scale(1.005)"
-});
-
-document.getElementById("experience_4").addEventListener("mouseout", event => {
-    document.getElementById("experience_4").style.transform = "scale(1)"
-});
-
