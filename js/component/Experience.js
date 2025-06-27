fc72855b787eeb7094f845742d5d1572d76d2cc3
Riki <rmcal024@uottawa.ca>
2025-06-27 14:06:43 -0400

    Experience update 2

--- js/component/Experience.js
+++ js/component/Experience.js
@@ -4,27 +4,27 @@ class Experience extends HTMLElement {
     }
     
     connectedCallback() {
-        var company = this.getAttribute('company');
-        var link = this.getAttribute('link');
-        var logo = this.getAttribute('logo');
-        var title = this.getAttribute('title');
-        var location = this.getAttribute('location');
-        var start = this.getAttribute('start');
-        var end = this.getAttribute('end');
+        var e_company = this.getAttribute('company');
+        var e_link = this.getAttribute('link');
+        var e_logo = this.getAttribute('logo');
+        var e_title = this.getAttribute('title');
+        var e_location = this.getAttribute('location');
+        var e_start = this.getAttribute('start');
+        var e_end = this.getAttribute('end');
 
         this.innerHTML = `
         <div class="experience_box">
             <div class="logo_box">
-                <img class="logo_experience" alt="experience_logo" src="assets/${logo}" title="${company}">
+                <img class="logo_experience" alt="experience_logo" src="assets/${e_logo}" title="${e_company}">
             </div>
             <div class="experience_box_details">
                 <div class="experience_box_left">
-                    <h4 id="title_text">${title}</h4>
-                    <a class="experience_link" href="${link}" target="_blank">${company}</a>
+                    <h4 id="title_text">${e_title}</h4>
+                    <a class="experience_link" href="${e_link}" target="_blank">${e_company}</a>
                 </div>
                 <div class="experience_box_right">
-                    <p id="title_text">${start} - ${end}</p>
-                    <p>${location}</p>
+                    <p id="title_text">${e_start} - ${e_end}</p>
+                    <p>${e_location}</p>
                 </div>
             </div>
         </div>
