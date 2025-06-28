83225562febbc564bcd737e757cd8c4e07277ff7
Riki <rmcal024@uottawa.ca>
2025-06-27 23:29:10 -0400

    Icon file fix
    * Icons were not loading correctly

--- js/component/Tools.js
+++ js/component/Tools.js
@@ -2,7 +2,7 @@ class Tools extends HTMLElement {
     connectedCallback() {
         const tool = this.getAttribute('tool');
         const type = this.getAttribute('type');
-        const src = `assets/icons/${tool}.png`;
+        const src = `assets/icons/${tool}.png`.toLowerCase();
         this.innerHTML = `
             <img class="${type}" src="${src}" alt="${tool}" title="${tool}">
         `;
