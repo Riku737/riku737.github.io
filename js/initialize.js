94e19d3ca2c6597383302cc26bae19bf42576f45
Riki <rmcal024@uottawa.ca>
2025-07-03 23:10:24 -0400

    js load fix

--- js/initialize.js
+++ js/initialize.js
@@ -4,15 +4,11 @@ const scriptsToLoad = [
     "js/component/Tools.js",
     "js/component/Projects.js",
     "js/component/Experience.js",
-    "js/component/Footer.js",
-    "js/animation.js",
-    "js/script.js",
-    "js/vanilla-tilt.js",
+    "js/component/Footer.js"
 ];
 
 scriptsToLoad.forEach(src => {
     const script = document.createElement('script');
     script.src = src;
-    script.defer = true; // Optional: preserves execution order
     document.head.appendChild(script);
 });
