6fc1f39356b2bf38b51ebbc6ab089beaee3e1a85
Riki <rmcal024@uottawa.ca>
2025-08-23 23:36:43 -0400

    icon fix

--- js/component/Tools.js
+++ js/component/Tools.js
@@ -2,7 +2,7 @@ class Tools extends HTMLElement {
 	connectedCallback() {
 		const tool = DOMPurify.sanitize(this.getAttribute("tool"));
 		const type = DOMPurify.sanitize(this.getAttribute("type"));
-		const src = `assets/icons/${tool}.png`.toLowerCase();
+		const src = `assets/icons/${tool.toLowerCase()}.png`;
 		this.innerHTML = `
             <div class="tool_item" title="${tool}">
                 <img class="${type}" src="${src}" alt="${tool}" title="${tool}">
